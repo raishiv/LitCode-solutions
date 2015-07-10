dba1337f44b602ec574bc06fe831108b5a281bc2
Shiv <raishiv@gmail.com>
2015-07-10 01:27:34 -0500

    solved palindrome in liked list with O(n) time and O(1) space

--- palindrome_linkList.cpp
+++ palindrome_linkList.cpp
@@ -11,47 +11,35 @@ public:
     bool isPalindrome(ListNode* head) {
         if(head == NULL) return true;
         else if(head->next == NULL) return true;
-        ListNode* h = head->next;
-        ListNode* t = head;
-        int count = 0;
-        
-        while(h->next != NULL){
-            if(t->val == h->val){
-                count = 1;
-                break;
-            }
-            else if(t->val = h->next->val){
-                h = h->next;
-                count = 1;
-                break;
-            }
-            t = t->next ;
-            h = h->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            slow=  slow->next;
+            fast = fast->next->next;
         }
-        if(count){
-            ListNode* cur = h;
-            ListNode* next;
-            ListNode* prev = NULL;
-            while(cur != NULL){
+        ListNode* cur = slow;
+        ListNode* next;
+        ListNode* prev = NULL;
+        while(cur != NULL){
                 next = cur->next;
                 cur->next = prev;
                 prev = cur;
                 cur = next;
-            }
-            h = prev;
-            ListNode* f = head;
-            while(h->next != NULL){
-                if(f->val == h->val){
+        }
+        ListNode* t = prev;
+        ListNode* f = head;
+        while(t != f && t && f){
+                cout<<"f:"<<f->val<<"t:"<<t->val<<endl;
+                if(f->val == t->val){
                     f = f->next;
-                    h = h->next;
+                    t = t->next;
                 }
                 else{
                     return false;
                 }
-                return true;
-            }
         }
-        else return false;
+        return true;
+        
         
     }
 };
