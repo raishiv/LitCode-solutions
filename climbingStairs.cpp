ed84ead80fd7d2834f52c2d79f70b5b976435482
Shiv <raishiv@gmail.com>
2015-07-12 00:04:18 -0500

    No. of ways to climb stairs with 1 and 2 steps at a time

---
+++ climbingStairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+    int a = 1, b = 1;
+    for (int i = 1;i < n;i++) {
+        b += a;
+        a = b - a;
+    }
+    return b;
+
+    }
+};
+
+//this is basically a fibonacci number.
