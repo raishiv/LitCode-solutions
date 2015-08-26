e3b3553187ec7a4c4f95249fc28655a52458e78c
Shiv <raishiv@gmail.com>
2015-08-26 00:31:29 -0500

    check whether the number is palindrome or not

---
+++ IsPalindrome.cpp
@@ -0,0 +1,15 @@
+//check whether the given number is palindrome or not in O(1) space
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0|| (x!=0 &&x%10==0)) return false;
+        int sum=0;
+        while(x>sum)
+        {
+            sum = sum*10+x%10;
+            x = x/10;
+        }
+        return (x==sum)||(x==sum/10);
+    }
+};
